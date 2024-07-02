1bdf11af97bae94f4a7ae4f7b9f77961bdadd07c
pyropecs <praveengopu0105@gmail.com>
2024-07-02 18:05:06 +0530

    new-todo

---
+++ index.js
@@ -0,0 +1,8 @@
+const tasks = [];
+const input = document.querySelector("#text-input");
+const form = document.querySelector("#submit-form")
+const p = document.querySelector(".task-name")
+form.addEventListener("submit",(e)=>{
+    e.preventDefault()
+    
+})
