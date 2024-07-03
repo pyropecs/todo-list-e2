feef2d4d673f8c7f2a05b18c2f66a0084b3f410f
pyropecs <praveengopu0105@gmail.com>
2024-07-03 10:43:48 +0530

    updated changes

--- index.js
+++
@@ -1,8 +0,0 @@
-const tasks = [];
-const input = document.querySelector("#text-input");
-const form = document.querySelector("#submit-form")
-const p = document.querySelector(".task-name")
-form.addEventListener("submit",(e)=>{
-    e.preventDefault()
-    
-})
