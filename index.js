dc6dc667bafdcbbf1534f7375c83f26299474a26
pyropecs <praveengopu0105@gmail.com>
2024-07-08 14:12:53 +0530

    localstorage

--- index.js
+++ index.js
@@ -3,9 +3,11 @@ const input = document.querySelector("#text-input");
 const todoList = document.querySelector("#task-list");
 const statusId = document.querySelector("#status-id");
 const taskCards = [];
+const tasks = [];
 form.addEventListener("submit", submitForm);
 todoList.addEventListener("click", operations);
 statusId.addEventListener("change", filterTodos);
+document.addEventListener("DOMContentLoaded", renderTasks);
 function submitForm(event) {
   event.preventDefault();
   const taskName = input.value;
@@ -13,19 +15,39 @@ function submitForm(event) {
   input.value = "";
 }
 function createTask(taskName) {
-  const taskCard = createTaskCard(taskName);
+  let taskId = 0;
+  const taskCard = createTaskCard(taskName, taskId);
+  const task = {
+    taskId,
+    taskName,
+    completed: false,
+  };
 
+  saveTodoToLocalStorage(task);
   taskCards.push(taskCard);
   todoList.append(taskCard);
-  renderTaskCards();
+
+  taskId++;
 }
-function renderTaskCards() {
-  return taskCards;
+function renderTasks() {
+  let todos;
+  if (localStorage.getItem("todos") === null) {
+    todos = [];
+  } else {
+    todos = JSON.parse(localStorage.getItem("todos"));
+  }
+
+  todos.forEach((todo) => {
+    const taskCard = createTaskCard(todo.taskName, todo.taskId, todo.completed);
+    taskCards.push(taskCard);
+    todoList.append(taskCard);
+  });
 }
-function createTaskCard(taskName) {
+function createTaskCard(taskName, taskId, isCompleted) {
   const taskCard = document.createElement("div");
   taskCard.classList.add("todo-card");
-  const checkBoxContainer = createCheckbox();
+  taskCard.setAttribute("index", taskId);
+  const checkBoxContainer = createCheckbox(isCompleted);
   const taskNameContainer = createTaskName(taskName);
   const btnGroup = createToDoButtons();
   taskCard.append(checkBoxContainer, taskNameContainer, btnGroup);
@@ -65,40 +87,45 @@ function createTaskName(taskName) {
   taskNameContainer.classList.add("task-name-container");
   const taskNameInput = document.createElement("input");
   taskNameInput.value = taskName;
-
+  taskNameInput.readOnly = true;
   taskNameInput.classList.add("task-name");
   taskNameInput.setAttribute("id", "task-name-id");
   taskNameContainer.append(taskNameInput);
   return taskNameContainer;
 }
 
-function createCheckbox() {
+function createCheckbox(isCompleted) {
   const checkBoxContainer = document.createElement("div");
   checkBoxContainer.classList.add("checkbox-container");
 
   const checkInput = document.createElement("input");
+
   checkInput.type = "checkbox";
   checkInput.setAttribute("id", "completed-checkbox");
   checkInput.classList.add("completed");
+  checkInput.checked = isCompleted;
   checkBoxContainer.append(checkInput);
   return checkBoxContainer;
 }
 function operations(e) {
   const target = e.target;
-  console.log(target.id);
+  const todoCard = target.parentNode.parentNode.parentNode;
+  const taskName = todoCard.querySelector("#task-name-id");
+  console.log(target);
   if (target.id === "completed-checkbox") {
     taskCompleted(e.target);
   }
-
   if (target.id === "edit-btn-id") {
-    const todoCard = target.parentNode.parentNode.parentNode;
-  const taskName = todoCard.querySelector("#task-name-id")
-  taskName.focus()
+    taskName.readOnly = false;
+    taskName.focus();
   }
 
   if (target.id === "delete-btn-id") {
     const todoCard = target.parentNode.parentNode.parentNode;
     todoCard.remove();
+  }
+  if (target.id !== "edit-btn-id" && target.id !== "delete-btn-id") {
+    taskName.readOnly = true;
   }
 }
 
@@ -115,15 +142,15 @@ function filterTodos() {
   if (selectedValue === "completed") {
     renderCompletedTodos();
   }
-  if(selectedValue === "assigned"){
+  if (selectedValue === "assigned") {
     renderAssignedTodos();
   }
 
-  if(selectedValue === "all"){
-    renderAllTodos()
+  if (selectedValue === "all") {
+    renderAllTodos();
   }
 }
-function renderAllTodos(){
+function renderAllTodos() {
   const completedCheckboxes = document.querySelectorAll("#completed-checkbox");
 
   completedCheckboxes.forEach((checkbox) => {
@@ -131,13 +158,10 @@ function renderAllTodos(){
     const todoCard = checkBoxContainer.parentElement;
     todoCard.classList.add("flex");
     todoCard.classList.remove("hide");
-    
   });
-
 }
 
-
-function renderAssignedTodos(){
+function renderAssignedTodos() {
   const completedCheckboxes = document.querySelectorAll("#completed-checkbox");
 
   completedCheckboxes.forEach((checkbox) => {
@@ -160,10 +184,37 @@ function renderCompletedTodos() {
     const todoCard = checkBoxContainer.parentElement;
     todoCard.classList.remove("flex");
     todoCard.classList.add("hide");
-  
+
     if (checkbox.checked) {
       todoCard.classList.remove("hide");
       todoCard.classList.add("flex");
     }
   });
+}
+function saveTodoToLocalStorage(task) {
+  let todos = [];
+  if (localStorage.getItem("todos") == null) {
+    todos = [];
+  } else {
+    todos = JSON.parse(localStorage.getItem("todos"));
+  }
+  todos.push(task);
+  localStorage.setItem("todos", JSON.stringify(todos));
+}
+
+function updateTodotoLocalStorage(task) {
+  const todos = JSON.parse(localStorage.getItem("todos"));
+
+  const updatedTodos = updateTask(todos, task);
+  localStorage.setItem("todos", JSON.stringify(updatedTodos));
+}
+function updateTask(todos, task) {
+  const newTodos = todos.map((todo) => {
+    if (todo.taskId === task.taskId) {
+      todo.taskName = task.taskName;
+      todo.completed = task.completed;
+    }
+    return todo;
+  });
+  return newTodos;
 }
