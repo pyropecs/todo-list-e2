a0bbba569a3cbb87a2b6614daec374e69435056f
pyropecs <praveengopu0105@gmail.com>
2024-07-10 09:48:49 +0530

    validation fix

--- validation.js
+++ validation.js
@@ -1,16 +1,17 @@
 export function validateText(input) {
-//to pass the all validation steps then only it will return true or it will give error message
+  //to pass the all validation steps then only it will return true or it will give error message
 
   if (validateNoEmptyString(input) !== true)
     return validateNoEmptyString(input);
-  if (validateOnlyAlphaNumeric(input) !== true)
-    return validateOnlyAlphaNumeric(input);
-
+  if (validateOnlyAlphaNumericAndAllowedSpecialCharacters(input) !== true)
+    return validateOnlyAlphaNumericAndAllowedSpecialCharacters(input);
+  if (validateThereisNoSpecialCharactersOnFirstLetter(input) !== true)
+    return validateThereisNoSpecialCharactersOnFirstLetter(input);
   if (validateNotMorThan150Characters(input) !== true)
     return validateNotMorThan150Characters(input);
 
-  if (validateMinimum10Characters(input) !== true)
-    return validateMinimum10Characters(input);
+  if (validateMinimumCharacters(input) !== true)
+    return validateMinimumCharacters(input);
   return true;
 }
 
@@ -24,7 +25,13 @@ function validateNoEmptyString(text) {
     return true;
   }
 }
-
+function validateThereisNoSpecialCharactersOnFirstLetter(text) {
+  //to validate that there is no special characters on the first letter
+  const re = /^[a-zA-Z0-9]+[^]*$/; //regular expression for only alphanumeric on the first letter
+  if (text.match(re)) {
+    return true;
+  } else return "First letter should be alphanumeric";
+}
 function validateNotMorThan150Characters(text) {
   // to check that more than 150 characters is not allowed
   const textLength = text.length;
@@ -35,9 +42,9 @@ function validateNotMorThan150Characters(text) {
   }
 }
 
-function validateOnlyAlphaNumeric(text) {
-//to check that only alphanumeric and special characters , ' - . is allowed
-  const alphaNumeric = /^[a-zA-Z0-9\s,'-.]+$/;
+function validateOnlyAlphaNumericAndAllowedSpecialCharacters(text) {
+  //to check that only alphanumeric and special characters , ' - . is allowed
+  const alphaNumeric = /^[a-zA-Z0-9\s,'-.]+$/; //regular expression for matching alphabets and  numbers and allowed special characters
 
   if (text.match(alphaNumeric)) {
     return true;
@@ -45,7 +52,8 @@ function validateOnlyAlphaNumeric(text) {
     return "Only alphanumeric and allowed special characters , ' -";
   }
 }
-function validateMinimum10Characters(text) {
+function validateMinimumCharacters(text) {
+  //to check that input has more than 10 characters
   const textLength = text.length;
   if (textLength > 10) {
     return true;
